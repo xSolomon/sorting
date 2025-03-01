443ce4751e451548fc7ad785ea0a60f4bb109f9f
xSolomon <12345procto@gmail.com>
2025-03-01 22:30:33 +0700

    add lesson 5 solution

--- sorting5/sorting5_1.py
+++ sorting5/sorting5_1.py
@@ -3,37 +3,44 @@
 def partition(values : list[int], left_border : int, right_border : int) -> int:
     ''' Split array values into two groups depending on pivot, and return pivot. '''
     while left_border < right_border:
-        pivot_index : int = (right_border - left_border) // 2
+        pivot_index : int = (right_border + left_border) // 2
         pivot : int = values[pivot_index]
         left_part_index = left_border
         right_part_index = right_border
-        while values[left_part_index] < pivot:
-            left_part_index += 1
-        while values[right_part_index] > pivot:
-            right_part_index -= 1
-        if left_part_index == right_part_index and \
-            values[left_part_index] > values[right_part_index]:
+        while left_part_index <= right_part_index:
+            while values[left_part_index] < pivot:
+                left_part_index += 1
+            while values[right_part_index] > pivot:
+                right_part_index -= 1
+            if left_part_index == right_part_index - 1 and \
+                values[left_part_index] > values[right_part_index]:
+                values[left_part_index], values[right_part_index] = \
+                    values[right_part_index], values[left_part_index]
+                break
+            if left_part_index == right_part_index or \
+                (left_part_index == right_part_index - 1 and \
+                    values[left_part_index] < values[right_part_index]):
+                return pivot_index
             values[left_part_index], values[right_part_index] = \
                 values[right_part_index], values[left_part_index]
-            break
-        if left_part_index == right_part_index or \
-            (left_part_index == right_part_index - 1 and \
-                values[left_part_index] < values[right_part_index]):
-            return pivot_index
-        values[left_part_index], values[right_part_index] = \
-            values[right_part_index], values[left_part_index]
-        if values[left_part_index] == pivot:
-            pivot_index = left_part_index
-            continue
-        if values[right_part_index] == pivot:
-            pivot_index = right_part_index
+            if values[left_part_index] == pivot:
+                pivot_index = left_part_index
+                continue
+            if values[right_part_index] == pivot:
+                pivot_index = right_part_index
     return None
-
 
 def QuickSort(array : list[int], left : int, right : int) -> None:
     ''' Quicksort using partition function. '''
-    if left == right:
+    if left >= right:
         return
     pivot : int = partition(array, left, right)
     QuickSort(array, left, pivot - 1)
     QuickSort(array, pivot + 1, right)
+
+
+
+
+
+
+
---
+++ sorting5/tests5_1.py
@@ -0,0 +1,52 @@
+''' Tests for lesson 5 solution. '''
+
+import unittest
+from typing import Tuple
+from sorting5_1 import partition, QuickSort
+
+class PartitionTests(unittest.TestCase):
+    ''' Tests for partition function. '''
+    def test_on_empty_array(self) -> None:
+        ''' Expected None as pivot, array shouldn't change. '''
+        values : list[int] = []
+        self.assertIsNone(partition(values, 0, 0))
+        self.assertEqual(values, [])
+
+    def test_on_seven_elements_array(self) -> None:
+        ''' Expected correct pivot and changes to array. '''
+        values : list[int] = [7, 5, 6, 4, 3, 1, 2]
+        results : Tuple[int, list[int]] = (3, [2, 1, 3, 4, 6, 5, 7])
+        with self.subTest():
+            self.assertEqual(partition(values, 0, len(values) - 1), results[0])
+            self.assertEqual(values, results[1])
+        values = [1, 3, 4, 6, 5, 2, 8]
+        results = (5, [1, 3, 4, 2, 5, 6, 8])
+        with self.subTest():
+            self.assertEqual(partition(values, 0, len(values) - 1), results[0])
+            self.assertEqual(values, results[1])
+
+class QuickSortTests(unittest.TestCase):
+    ''' Tests for QuickSort function. '''
+    def test_on_empty_array(self) -> None:
+        ''' Array shouldn't change. '''
+        values : list[int] = []
+        QuickSort(values, 0, len(values) - 1)
+        self.assertEqual(values, [])
+
+    def test_on_predefined_arrays(self) -> None:
+        ''' Each array should be sorted as the result. '''
+        unsorted_arrays : list[list[int]] = [
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+            [-525215, 1221, 677, 0, 111111, 32233, -6645332, 11343224, -2323523525252, 14141426474858],
+            [0],
+            [1, 2],
+            [2, 1]
+        ]
+        for array in unsorted_arrays:
+            sorted_array : list[int] = sorted(array)
+            with self.subTest():
+                QuickSort(array, 0 , len(array) - 1)
+                self.assertEqual(array, sorted_array)
+
+unittest.main()
