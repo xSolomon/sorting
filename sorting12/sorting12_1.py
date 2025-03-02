5cd98de952f9054160251186952ff256dc1cdc49
xSolomon <12345procto@gmail.com>
2025-03-03 04:53:57 +0700

    add lesson 12 solution

---
+++ sorting12/sorting12_1.py
@@ -0,0 +1,67 @@
+''' Lesson 12 solution. '''
+
+class BinarySearch():
+    ''' Searches element in sorted array in log(N) time. '''
+    def __init__(self, ary_sorted_ascending : list[int]):
+        self._values = ary_sorted_ascending.copy() # List of ascending sorted values.
+        self.Left : int = 0 # Left border.
+        self.Right : int = len(self._values) - 1 # Right border.
+        self._search_result : int = 0 # Current search status
+
+    def Step(self, N : int) -> None:
+        ''' Performs one step in searching value. '''
+        if self._search_result != 0:
+            return
+        if not self._values:
+            self._search_result = -1
+            return
+        middle_element_index : int = (self.Left + self.Right) // 2
+        middle_element : int = self._values[middle_element_index]
+        match (N > middle_element) - (N < middle_element):
+            case 0:
+                self.Left = self.Right = middle_element_index
+                self._search_result = 1
+                return
+            case -1:
+                self.Right = middle_element_index - 1
+            case 1:
+                self.Left = middle_element_index + 1
+        if self.Right < self.Left:
+            self._search_result = -1
+
+    def GetResult(self) -> int:
+        ''' Returns currect search status, where:
+            0 = search continues
+            1 = element found
+            -1 = element not found '''
+        return self._search_result
+
+    def GallopingSearch(self, ary_sorted_ascending : list[int], search_value : int) -> bool:
+        ''' Performs search with O(log(i)) complexity. '''
+        if len(ary_sorted_ascending) < 1:
+            return False
+        i : int = 1
+        current_index : int = 0
+        while ary_sorted_ascending[current_index] < search_value:
+            i += 1
+            current_index = pow(2, i) - 2
+            if current_index >= len(ary_sorted_ascending):
+                current_index = len(ary_sorted_ascending) - 1
+                break
+        if ary_sorted_ascending[current_index] == search_value:
+            return True
+        binary_searh : BinarySearch = BinarySearch(ary_sorted_ascending)
+        binary_searh.Left = pow(2, i - 1) - 1
+        binary_searh.Right = current_index
+        while binary_searh.GetResult() == 0:
+            binary_searh.Step(search_value)
+        return binary_searh.GetResult() == 1
+
+
+
+
+
+
+
+
+
---
+++ sorting12/tests12_1.py
@@ -0,0 +1,41 @@
+''' Tests for lesson 12 solution. '''
+
+import unittest
+from sorting12_1 import BinarySearch
+
+class GallopingSearchTests(unittest.TestCase):
+    ''' Tests for GallopingSearch function of BinarySearch class. '''
+    def setUp(self) -> None:
+        ''' Test preparations. '''
+        self.binary_searh : BinarySearch = BinarySearch([])
+
+    def tearDown(self) -> None:
+        ''' Aftertest cleanup. '''
+        self.binary_searh = None
+
+    def test_on_empty_ary(self) -> None:
+        ''' Must return false. '''
+        self.assertFalse(self.binary_searh.GallopingSearch([], 0))
+
+    def test_on_one_element_ary(self) -> None:
+        ''' Expected true when searching for this single element.
+            false otherwise. '''
+        values_to_search : list[int] = [-5, -1, 0, 1, 2, 3, 4, 5]
+        ary : list[int] = [3]
+        for search_value in values_to_search:
+            expected_result : bool = ary[0] == search_value
+            with self.subTest(search_value = search_value):
+                self.assertEqual(self.binary_searh.GallopingSearch(ary, search_value),
+                    expected_result)
+
+    def test_on_seven_element_ary(self) -> None:
+        ''' Expected true when key exists, false otherwise. '''
+        values_to_search : list[int] = [-341414, 463663, -5, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        ary : list[int] = [1, 2, 3, 4, 5, 6, 7]
+        for search_value in values_to_search:
+            expected_result : bool = search_value in ary
+            with self.subTest(search_value = search_value):
+                self.assertEqual(self.binary_searh.GallopingSearch(ary, search_value),
+                    expected_result)
+
+unittest.main()
